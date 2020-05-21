2e182f19896061aa0c7e762a7cc93102a68408f6
Vostromus <vostromus@gmail.com>
2020-05-20 21:31:24 -0400

    Gold Star 1

--- src/main/java/AdventDay_2.java
+++ src/main/java/AdventDay_2.java
@@ -1,5 +1,33 @@
+import java.util.List;
+
 public class AdventDay_2{
     public String returnTestString(){
         return "Advent Day Two Test!";
     }
+
+	public List<Integer> runOpcode(List<Integer> opcode) {
+        
+        Boolean solving = true;        
+        int currentIdx = 0;
+        int opCodeIncrement = 4;
+        while(solving){
+            switch(opcode.get(currentIdx)){
+                case 1:                    
+                    opcode.set( opcode.get((currentIdx + 3)), opcode.get(opcode.get(currentIdx + 1)) + opcode.get(opcode.get(currentIdx + 2)));
+                    currentIdx += opCodeIncrement;
+                break;
+                case 2:
+                    opcode.set( opcode.get((currentIdx + 3)), opcode.get(opcode.get(currentIdx + 1)) * opcode.get(opcode.get(currentIdx + 2)));
+                    currentIdx += opCodeIncrement;
+                break;
+                case 99:
+                    solving = false;
+                break;
+                default:
+                    solving = false;
+            }
+        }
+
+        return opcode;
+	}
 }
--- src/test/java/AdventDay_2_Test.java
+++ src/test/java/AdventDay_2_Test.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +12,35 @@ public class AdventDay_2_Test {
     void returnsAdventDayTwoTest(){
         AdventDay_2 advent = new AdventDay_2();
         assertEquals("Advent Day Two Test!", advent.returnTestString());
+    }
+
+    @Test
+    @DisplayName("Test Opcode")
+    void returnRunOpcode(){
+        AdventDay_2 adventDay_2 = new AdventDay_2();
+        List<Integer> opcode = Arrays.asList(1,0,0,3,99);
+        List<Integer> result = Arrays.asList(1,0,0,2,99);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
+
+        opcode = Arrays.asList(1,9,10,3,2,3,11,0,99,30,40,50);
+        result = Arrays.asList(3500,9,10,70,2,3,11,0,99,30,40,50);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
+
+        opcode = Arrays.asList(1,0,0,0,99);
+        result = Arrays.asList(2,0,0,0,99);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
+
+        opcode = Arrays.asList(2,3,0,3,99);
+        result = Arrays.asList(2,3,0,6,99);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
+
+        opcode = Arrays.asList(2,4,4,5,99,0);
+        result = Arrays.asList(2,4,4,5,99,9801);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
+
+        opcode = Arrays.asList(1,1,1,4,99,5,6,0,99);
+        result = Arrays.asList(30,1,1,4,2,5,6,0,99);
+        assertEquals(result,adventDay_2.runOpcode(opcode));
     }
 
 }
